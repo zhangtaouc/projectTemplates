b7fb680026b3d515fc90d7d675a06c2a0a4910f1
Max <3131966940@qq.com>
2025-05-25 22:43:13 +0800

    解决类型报错

--- vue3-ts/src/vite-env.d.ts
+++ vue3-ts/src/vite-env.d.ts
@@ -1,1 +1,18 @@
-/// <reference types="vite/client" />
+//该文件主要是为了解决模块未定义导致的ts报错
+//tsconfig.app.json指定编译src/下的ts文件,该文件位置不可移动
+
+//定义所有的css
+declare module '*.css'
+//定义vuex
+declare module 'vuex' {
+  export * from 'vuex/types/index.d.ts'
+  export * from 'vuex/types/helpers.d.ts'
+  export * from 'vuex/types/logger.d.ts'
+  export * from 'vuex/types/vue.d.ts'
+}
+//定义所有的vue文件
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
